d4f0d56bc13e9c064e0d0015bd0b3d853d61c6c5
Manasa1010 <64597788+Manasa1010@users.noreply.github.com>
2020-06-25 22:23:12 +0530

    Add files via upload

--- dustbin.js
+++ dustbin.js
@@ -6,20 +6,14 @@ class Rect{
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-        this.image=loadImage("dustbin.png");
+        
         World.add(world, this.body);
       }
       display(){
-        var angle = this.body.angle;
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
-       // rectMode(CENTER);
-      // rect(this.image,0,0,this.width,this.height);
-        pop();
-        image(this.image,490,300,100,100);
-        image(this.image,510,300,140,100);
-        image(this.image,650,300,20,100);
- 
+        var pos=this.body.position;
+       rectMode(CENTER);
+       rect(pos.x,pos.y,this.width,this.height);
+       
+        
       }
 }
--- sketch.js
+++ sketch.js
@@ -4,6 +4,9 @@ const Bodies=Matter.Bodies;
 const Constraint=Matter.Constraint;
 
 var paper;
+function preload(){
+  dust=loadImage("dustbin.png");
+}
 
 
 function setup() {
@@ -14,9 +17,10 @@ function setup() {
  paper = new Paper(200,100);
  launcher = new Launcher(paper.body,{x:100,y:180});
 
-
+ rect1=new Rect(520,350,20,140);
+ rect2=new Rect(650,350,20,140);
 floor=new Floor(800,390,1600,20);
-rect1=new Rect(500,350,20,100)
+
  
 }
 
@@ -28,9 +32,11 @@ function draw() {
  //rect1.display();
  //rect2.display();
  rect1.display();
+
+ rect2.display();
  floor.display();
  
-
+image(dust,350,220,400,250);
 
   drawSprites();
 }
